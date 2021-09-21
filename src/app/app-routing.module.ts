b69a7de9f8eb8a60b29fdba18d65650607919e1f
John Garzon <gajohn511@gmail.com>
2021-09-21 01:27:06 -0400

    added routing to products component, as home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { ProductsComponent } from "./products/products.component";
+
+const routes: Routes = [{ path: "", component: ProductsComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
